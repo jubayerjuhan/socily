af81f7b64f03ff0bafc5e038038a514d2f289cc6
Jubayer Juhan <davidjuhan23@gmail.com>
2022-09-20 20:29:10 +0600

    Git Pulled and merged (Login and Otp working)

--- src/typings/redux-typings/redux-typings.d.ts
+++ src/typings/redux-typings/redux-typings.d.ts
@@ -6,12 +6,9 @@ export interface State {
     user: User;
     error: string;
     jwtToken: string;
-<<<<<<< HEAD
     otpError: string;
     otpSuccess: boolean;
     otpLoading: boolean;
-=======
->>>>>>> 1148e39152abdd89b9b3dee19d92480d056df1f7
   };
   theme: Theme;
 }
